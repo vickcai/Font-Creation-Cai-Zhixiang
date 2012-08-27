d535e765f2373da41fa375911f944730f67a4af3
Vick Cai Zhixiang <cai_zhixiang_vick@hotmail.com>
2012-08-27 08:40:15 +0800

    Front 6

---
+++ Font01_Normal_L_Cai_Zhixiang_jpg/Font01_Normal_L_Cai_Zhixiang_jpg.pde
@@ -0,0 +1,16 @@
+
+void setup(){
+size(400,400);
+
+}
+
+void draw(){
+
+background(200);
+fill(255);
+stroke(255);
+rect(150,50,50,300);
+rect(150,300,150,50);
+
+
+}
---
+++ Front06_line_L_Cai_Zhixiang/applet/Front06_line_L_Cai_Zhixiang.java
@@ -0,0 +1,41 @@
+import processing.core.*; 
+import processing.xml.*; 
+
+import java.applet.*; 
+import java.awt.Dimension; 
+import java.awt.Frame; 
+import java.awt.event.MouseEvent; 
+import java.awt.event.KeyEvent; 
+import java.awt.event.FocusEvent; 
+import java.awt.Image; 
+import java.io.*; 
+import java.net.*; 
+import java.text.*; 
+import java.util.*; 
+import java.util.zip.*; 
+import java.util.regex.*; 
+
+public class Front06_line_L_Cai_Zhixiang extends PApplet {
+
+public void setup(){
+size(400,400);
+
+}
+
+public void draw(){
+
+background(200);
+fill(255);
+stroke(255);
+line(150,50,150,350);
+line(150,50,200,50);
+line(200,50,200,300);
+line(150,350,300,350);
+line(300,350,300,300);
+line(300,300,200,300);
+
+}
+  static public void main(String args[]) {
+    PApplet.main(new String[] { "--bgcolor=#F0F0F0", "Front06_line_L_Cai_Zhixiang" });
+  }
+}
