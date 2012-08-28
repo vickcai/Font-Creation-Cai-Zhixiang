efe3f0ce49a4fd661986da7e101266ffcde6f28f
Vick Cai Zhixiang <cai_zhixiang_vick@hotmail.com>
2012-08-28 11:58:22 +0800

    new

--- Font01_random_size_of_squarel_L_Cai_Zhixiang/applet/Font01_random_size_of_squarel_L_Cai_Zhixiang.java
+++ Font01_random_size_of_squarel_L_Cai_Zhixiang/applet/Font01_random_size_of_squarel_L_Cai_Zhixiang.java
@@ -15,31 +15,43 @@ import java.util.*;
 import java.util.zip.*; 
 import java.util.regex.*; 
 
-public class Font01_Normal_L_Cai_Zhixiang extends PApplet {
-
+public class Font01_random_size_of_squarel_L_Cai_Zhixiang extends PApplet {
+  public void setup() {
 /**
- White strokeless L: A basic "L"
+ A L constructed using random size of square.
  Cai Zhixiang 28/08/2012
  a0087731@nus.edu.sg
  */
  
-public void setup(){
-size(400,400);
 
-}
-
-public void draw(){
-
-background(200);
-fill(255);
+size(400, 400);
+background(220);
+smooth();
+fill(0);
+int x;
+int y;
+fill(8,245,245);
 stroke(255);
-rect(150,50,50,300);
-rect(150,300,150,50);
+for (x=0;x<15;x++) {
+  for (y=0;y<4;y++) {
+    rect(x*12+130, y*12+300, 8, 8);
+    strokeWeight(random(0, 5));
+  }
+}
+fill(8,245,245);
+stroke(255);
+for (x=0;x<5;x++) {
+  for (y=0;y<20;y++) {
+    rect(x*12+130, y*12+60, 8, 8);
+    strokeWeight(random(0, 5));
+  }
+}
 
 // Saves each frame as screen-0000.tif, screen-0001.tif, etc.
   saveFrame("Font1.jpg"); 
-}
+  noLoop();
+} 
   static public void main(String args[]) {
-    PApplet.main(new String[] { "--bgcolor=#F0F0F0", "Font01_Normal_L_Cai_Zhixiang" });
+    PApplet.main(new String[] { "--bgcolor=#F0F0F0", "Font01_random_size_of_squarel_L_Cai_Zhixiang" });
   }
 }
---
+++ Font01_random_size_of_squarel_L_Cai_Zhixiang/applet/Font01_random_size_of_squarel_L_Cai_Zhixiang.pde
@@ -0,0 +1,32 @@
+/**
+ A L constructed using random size of square.
+ Cai Zhixiang 28/08/2012
+ a0087731@nus.edu.sg
+ */
+ 
+
+size(400, 400);
+background(220);
+smooth();
+fill(0);
+int x;
+int y;
+fill(8,245,245);
+stroke(255);
+for (x=0;x<15;x++) {
+  for (y=0;y<4;y++) {
+    rect(x*12+130, y*12+300, 8, 8);
+    strokeWeight(random(0, 5));
+  }
+}
+fill(8,245,245);
+stroke(255);
+for (x=0;x<5;x++) {
+  for (y=0;y<20;y++) {
+    rect(x*12+130, y*12+60, 8, 8);
+    strokeWeight(random(0, 5));
+  }
+}
+
+// Saves each frame as screen-0000.tif, screen-0001.tif, etc.
+  saveFrame("Font1.jpg"); 
